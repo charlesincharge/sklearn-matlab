83de22d78f56518db989e60a5774a73f41d3d593
Charles Guan <charlesguan94@gmail.com>
2019-09-30 16:04:30 -0700

    Initial partial least squares regression (PLSR) draft. Work-in-progress

---
+++ lib/cross_decomposition/PLSR.m
@@ -0,0 +1,46 @@
+classdef PLSR < BaseEstimator & TransformerMixin & RegressorMixin
+    % Partial least squares regression(PLSR).
+    
+    properties (GetAccess = 'public', SetAccess = 'public')
+        % parameters
+        n_components = 2; % Number of components to keep.
+    end
+    
+    properties (GetAccess = 'public', SetAccess = 'private')
+        % attributes
+        x_loadings; % X block loadings vectors.
+        y_loadings; % Y block loadings vectors.
+        x_scores; % X, dimensionality-reduced
+        y_scores; % Y, dimensionality-reduced
+        x_rotations; % mapping from X to latents
+        y_rotations; % mapping from Y to latents
+    end
+    
+    methods
+        % constructor
+        function obj = PCA_(params)
+            if nargin>0
+                obj.set_params(params)
+            end
+        end
+        
+        % Fit the model with X and Y.
+        function fit(obj,X,Y,~)
+            [obj.x_loadings, obj.y_loadings, obj.x_scores, obj.y_scores] = ...
+                plsregress(X, Y, obj.n_components);
+        end
+        
+        % Apply the dimensionality reduction on X and Y (optional).
+        function [X_new, Y_new] = transform(obj,X,Y)
+            % check_is_fitted(obj) % as in sklearn
+            
+            if exists('Y', 'var')
+            end
+        end
+        
+        % Apply the dimension reduction learned from the training data
+        function C = predict(obj,X)
+%             C = X*obj.coef_ + obj.intercept_; TODO fix
+        end
+    end
+end
